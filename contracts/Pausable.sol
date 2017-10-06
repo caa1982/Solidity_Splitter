3172f6ecfea7afb4daf6cadcc5f4b79e72ad9e14
caa1982 <cyilalvarezadriaansen@gmail.com>
2017-10-06 12:42:29 +0200

    change from isAdmin to isOwner

--- contracts/Pausable.sol
+++ contracts/Pausable.sol
@@ -5,17 +5,17 @@ contract Pausable {
     bool public contractPaused;
     address public owner;
 
-    modifier isAdmin() {
+    modifier isOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    function pauseContract() isAdmin public returns (bool success){
+    function pauseContract() isOwner public returns (bool success){
         contractPaused = !contractPaused;
         return true;
     }
 
-    function emergencyWithdrawal() isAdmin public returns (bool success) {
+    function emergencyWithdrawal() isOwner public returns (bool success) {
         require(contractPaused);
 
         msg.sender.transfer(this.balance);
